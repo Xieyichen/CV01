b32a5855d0f3b8b1bbe182bbc50bafa0d0e402dd
Xieyichen <yichen.xie@stud.tu-darmstadt.de>
2022-11-02 19:01:16 +0100

    p2

--- A1/problem2.py
+++ A1/problem2.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.ndimage import convolve
-
+import matplotlib.pyplot as plt
 
 def loaddata(path):
     """ Load bayerdata from file
@@ -32,7 +32,19 @@ def separatechannels(bayerdata):
     #
     # You code here
     #
+    g_channel = np.copy(bayerdata)
+    g_channel[::2, 1::2] = 0
+    g_channel[1::2, ::2] = 0
 
+    r_channel = np.copy(bayerdata)
+    r_channel[::, ::2] = 0
+    r_channel[1::2, 1::2] = 0
+
+    b_channel = np.copy(bayerdata)
+    b_channel[::, 1::2] = 0
+    b_channel[::2, ::2] = 0
+
+    return r_channel, g_channel, b_channel
 
 def assembleimage(r, g, b):
     """ Assemble separate channels into image
@@ -46,6 +58,9 @@ def assembleimage(r, g, b):
     #
     # You code here
     #
+    c = np.dstack((r, g, b))
+    return c
+
 
 
 def interpolate(r, g, b):
@@ -61,3 +76,33 @@ def interpolate(r, g, b):
     #
     # You code here
     #
+    kr = np.array([[1, 1, 1], [1, 1, 0], [1, 0, 0]])
+    kg = np.array([[1, 0, 0], [1, 1, 0], [1, 0, 0]])
+    kb = np.array([[1, 1, 1], [1, 1, 0], [1, 1, 0]])
+    r = convolve(r, kr, mode='constant', cval=0.0)
+    g = convolve(g, kg, mode='constant', cval=0.0)
+    b = convolve(b, kb, mode='constant', cval=0.0)
+    c = np.dstack((r, g, b))
+    return c
+
+
+
+img = loaddata('data/bayerdata.npy')
+
+r_channel, g_channel, b_channel = separatechannels(img)
+c_img_np = assembleimage(r_channel, g_channel, b_channel)
+#imgplot = plt.imshow(c_img_np)
+#plt.show()
+
+fin_img_np = interpolate(r_channel, g_channel, b_channel)
+#imgplot = plt.imshow(fin_img_np)
+#plt.show()
+
+#tune weights
+plt.rcParams["figure.figsize"] = [20.00, 10.00]
+plt.rcParams["figure.autolayout"] = True
+plt.subplot(1, 2, 1)
+plt.imshow(c_img_np)
+plt.subplot(1, 2, 2)
+plt.imshow(fin_img_np)
+plt.show()
